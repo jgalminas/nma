6ecf264eee93ee6d8ee07283d60a2685a90b61c1
Justas Galminas <j.galminas@gmail.com>
2023-01-30 13:32:52 +0000

    Basic canvas + image exporting functionality

--- Source Code/Android/app/src/main/java/com/example/nmadrawingapp/MainActivity.java
+++ Source Code/Android/app/src/main/java/com/example/nmadrawingapp/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.nmadrawingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import com.example.nmadrawingapp.model.data_sources.db.Database;
 import com.example.nmadrawingapp.model.data_sources.db.entitites.Image;
 import com.example.nmadrawingapp.model.repositories.ImageRepository;
+import com.example.nmadrawingapp.views.CanvasView;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,6 +23,7 @@ import dagger.hilt.android.AndroidEntryPoint;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private CanvasView canvas;
 
     @Inject
     ImageRepository imageRepository; // for testing purposes, this should be used in the viewmodel
@@ -29,13 +34,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.btn);
+        canvas = findViewById(R.id.canvas);
 
         //region room read example
 
         LiveData<List<Image>> imageList = imageRepository.getAllImages();
 
         imageList.observe(this, images -> {
-            button.setText(String.valueOf(images.size()));
+            button.setText("# of images in db: " + images.size());
         });
 
         //endregion
@@ -43,10 +49,7 @@ public class MainActivity extends AppCompatActivity {
         //region room write example
 
         button.setOnClickListener(button -> {
-
-            byte[] bytes = { 90, 20, 40, 10, 20, 40, 33 };
-            imageRepository.insertImage(new Image(UUID.randomUUID(), ".webp", bytes));
-
+            imageRepository.insertImage(new Image(UUID.randomUUID(), ".webp", canvas.getImageBytes()));
         });
 
         //endregion
---
+++ Source Code/Android/app/src/main/java/com/example/nmadrawingapp/views/CanvasView.java
@@ -0,0 +1,150 @@
+package com.example.nmadrawingapp.views;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import java.io.ByteArrayOutputStream;
+
+public class CanvasView extends View {
+
+    public static class BrushSettings {
+
+        private final Paint paint = new Paint();
+
+        private BrushSettings() {
+            paint.setAntiAlias(true);
+            paint.setDither(true);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeCap(Paint.Cap.ROUND);
+            paint.setStrokeWidth(15);
+        }
+
+        public void setColor(int color) {
+            paint.setColor(color);
+        }
+
+        public void setWidth(float width) {
+            paint.setStrokeWidth(width);
+        }
+
+        public int getColor() {
+            return paint.getColor();
+        }
+
+        public float getWidth() {
+            return paint.getStrokeWidth();
+        }
+
+        private Paint getPaint() {
+            return paint;
+        }
+    }
+
+    private Canvas canvas;
+    private Bitmap bitmap;
+    private Path path;
+    private final BrushSettings brushSettings = new BrushSettings();
+
+    private float pointerX, pointerY;
+    private static final float TOUCH_TOLERANCE = 4;
+
+    public CanvasView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+
+        canvas = new Canvas();
+        path = new Path();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int prevW, int prevH) {
+        super.onSizeChanged(w, h, prevW, prevH);
+
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(bitmap, 0, 0, brushSettings.getPaint());
+        canvas.drawPath(path, brushSettings.getPaint());
+    }
+
+    private void onTouchStart(float x, float y) {
+        path.reset();
+        path.moveTo(x, y);
+        pointerX = x;
+        pointerY = y;
+    }
+
+    private void onTouchMove(float x, float y) {
+
+        float diffX = Math.abs(x - pointerX);
+        float diffY = Math.abs(y - pointerY);
+
+        if (diffX >= TOUCH_TOLERANCE || diffY >= TOUCH_TOLERANCE) {
+            path.quadTo(pointerX, pointerY, (x + pointerX)/2, (y + pointerY)/2);
+            pointerX = x;
+            pointerY = y;
+        }
+    }
+
+    private void onTouchStop() {
+        path.lineTo(pointerX, pointerY);
+        canvas.drawPath(path, brushSettings.getPaint());
+        path = new Path();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                onTouchStart(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                onTouchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                onTouchStop();
+                invalidate();
+                break;
+        }
+
+        return true;
+    }
+
+    public byte[] getImageBytes() {
+
+        //TODO might have to move this to another thread, look into that a bit more
+        //TODO could potentially scale the bitmap down to reduced the size even more
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.WEBP, 100, stream);
+
+
+        return stream.toByteArray();
+    }
+
+    public BrushSettings getBrushSettings() {
+        return brushSettings;
+    }
+
+}
