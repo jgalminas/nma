ae2fd1190864ba239d39cbed72c7ae57ce680a4a
Justas Galminas <j.galminas@gmail.com>
2023-02-02 11:11:37 +0000

    Configure view binding

--- Source Code/Android/app/src/main/java/com/example/nmadrawingapp/MainActivity.java
+++ Source Code/Android/app/src/main/java/com/example/nmadrawingapp/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.lifecycle.LiveData;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
+
+import com.example.nmadrawingapp.databinding.ActivityMainBinding;
 import com.example.nmadrawingapp.model.data_sources.db.Database;
 import com.example.nmadrawingapp.model.data_sources.db.entitites.Image;
 import com.example.nmadrawingapp.model.repositories.ImageRepository;
@@ -22,34 +24,31 @@ import dagger.hilt.android.AndroidEntryPoint;
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private CanvasView canvas;
+    private ActivityMainBinding binding;
 
     @Inject
-    ImageRepository imageRepository; // for testing purposes, this should be used in the viewmodel
+    ImageRepository imageRepository; // for testing purposes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        button = findViewById(R.id.btn);
-        canvas = findViewById(R.id.canvas);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         //region room read example
 
         LiveData<List<Image>> imageList = imageRepository.getAllImages();
 
         imageList.observe(this, images -> {
-            button.setText("# of images in db: " + images.size());
+            binding.btn.setText("# of images in db: " + images.size());
         });
 
         //endregion
 
         //region room write example
 
-        button.setOnClickListener(button -> {
-            imageRepository.insertImage(new Image(UUID.randomUUID(), ".webp", canvas.getImageBytes()));
+        binding.btn.setOnClickListener(button -> {
+            imageRepository.insertImage(new Image(UUID.randomUUID(), ".webp", binding.canvas.getImageBytes()));
         });
 
         //endregion
