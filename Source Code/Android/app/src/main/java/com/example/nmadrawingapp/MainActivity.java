ac0bc35e84f8d617561df537277545b11cc888ea
Justas Galminas <j.galminas@gmail.com>
2023-02-04 10:49:53 +0000

    Configured navigation
    Set up navigation using Android Navigation Component.

--- Source Code/Android/app/src/main/java/com/example/nmadrawingapp/MainActivity.java
+++ Source Code/Android/app/src/main/java/com/example/nmadrawingapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.nmadrawingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
@@ -21,10 +24,6 @@ import dagger.hilt.android.AndroidEntryPoint;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private Dialog dialog;
-
-    @Inject
-    ImageRepository imageRepository; // for testing purposes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,100 +31,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //region room read example
-
-        LiveData<List<Image>> imageList = imageRepository.getAllImages();
-
-        imageList.observe(this, images -> {
-            binding.btn.setText("# of images in db: " + images.size());
-        });
-
-        //endregion
-
-
-        binding.btn.setOnClickListener(button -> {
-            showDialog();
-        });
-
-    }
-
-    private Dialog createDialog() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        LayoutInflater inflater = this.getLayoutInflater();
-
-        builder.setView(inflater.inflate(R.layout.save_image_dialog, null));
-
-        return builder.create();
-    }
-
-    private void showDialog() {
-
-        dialog = createDialog();
-
-        dialog.show(); // must show dialog before registering listeners
-
-        EditText ageInput = dialog.findViewById(R.id.age_input);
-
-        // register on cancel listener
-        dialog.findViewById(R.id.cancel_button).setOnClickListener(button -> {
-            dialog.cancel();
-        });
-
-        // register on save listener
-        dialog.findViewById(R.id.save_button).setOnClickListener(button -> {
-
-            int age = parseAge(ageInput.getText().toString());
-            boolean ageIsValid = validateAge(age);
-
-            if (ageIsValid) {
-                imageRepository.insertImage(new Image(
-                        age,
-                        binding.canvas.getImageBytes()
-                ));
-
-                binding.canvas.clearCanvas();
-                dialog.dismiss();
-
-            }
-
-        });
-
-        // validate age input on text change
-        ageInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable text) {
-                if (validateAge(parseAge(text.toString()))) {
-                    dialog.findViewById(R.id.error_message).setVisibility(View.GONE);
-                } else {
-                    dialog.findViewById(R.id.error_message).setVisibility(View.VISIBLE);
-                }
-            }
-        });
-
-    }
-
-    private boolean validateAge(int age) {
-        return age > 0 && age <= 130;
-    }
-
-    private int parseAge(String text) {
-        try {
-            return Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            return 0;
-        }
     }
 
 }
---
+++ Source Code/Android/app/src/main/java/com/example/nmadrawingapp/views/UploadFragment.java
@@ -0,0 +1,44 @@
+package com.example.nmadrawingapp.views;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nmadrawingapp.R;
+import com.example.nmadrawingapp.databinding.FragmentUploadBinding;
+
+public class UploadFragment extends Fragment {
+
+    private FragmentUploadBinding binding;
+
+    public UploadFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentUploadBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+}
