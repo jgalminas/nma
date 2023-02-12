470de566cc87ea28958d93e91b4baddef6c8fc60
Justas Galminas <j.galminas@gmail.com>
2023-02-12 12:44:10 +0000

    Created & styled the layout

---
+++ Source Code/Android/app/src/main/java/com/example/nmadrawingapp/views/StartFragment.java
@@ -0,0 +1,44 @@
+package com.example.nmadrawingapp.views;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nmadrawingapp.R;
+import com.example.nmadrawingapp.databinding.FragmentStartBinding;
+
+public class StartFragment extends Fragment {
+
+    private FragmentStartBinding binding;
+
+    public StartFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentStartBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+}
