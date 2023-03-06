b7c873b61e9fe1d38781d9834675df41ad570775
emma-freeman-plym <emma.freeman@students.plymouth.ac.uk>
2023-03-06 11:08:03 +0000

    Add CRUD operations to Location controller

--- Source Code/API/Controllers/LocationController.cs
+++ Source Code/API/Controllers/LocationController.cs
@@ -1,4 +1,7 @@
+using API.Exceptions;
 using API.Models;
+using API.Models.DTOs;
+using API.Models.Responses;
 using API.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,10 +18,80 @@ namespace API.Controllers
         {
             _locationService = locationService;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLocation([FromForm] LocationDTO data)
+        {
+            try
+            {
+                return Ok(new IdResponse()
+                {
+                    Id = await _locationService.CreateLocationAsync(data),
+                    Message = "OK",
+                });
+            }
+            catch (NotFound e)
+            {
+                return BadRequest(new GenericResponse() { Message = e.Message });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new GenericResponse() { Message = e.Message });
+            }
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetLocations()
         {
             return Ok(await _locationService.GetLocationsAsync());
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> GetLocation(int id)
+        {
+            try
+            {
+                return Ok(await _locationService.GetLocationByIdAsync(id));
+            }
+            catch (NotFound e)
+            {
+                return BadRequest(new GenericResponse() { Message = e.Message });
+            }
+        }
+
+        [HttpPatch]
+        [Route("{id:int}")]
+        public async Task<IActionResult> UpdateLocation(int id, [FromForm] LocationDTO data)
+        {
+            try
+            {
+                await _locationService.UpdateLocationAsync(id, data);
+                return Ok();
+            }
+            catch (NotFound e)
+            {
+                return BadRequest(new GenericResponse() { Message = e.Message });
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> DeleteLocation(int id)
+        {
+            try
+            {
+                await _locationService.DeleteLocationAsync(id);
+                return Ok();
+            }
+            catch (NotFound e)
+            {
+                return BadRequest(new GenericResponse() { Message = e.Message });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new GenericResponse() { Message = e.Message });
+            }
         }
     }
 }
