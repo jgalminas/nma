b249617d831d128a41955628f23cd1942fba1a49
Justas Galminas <j.galminas@gmail.com>
2023-04-05 14:35:54 +0100

    Implemented breadth section and the navigation between sections functionality

--- Source Code/Admin/src/components/Checkbox.tsx
+++ Source Code/Admin/src/components/Checkbox.tsx
@@ -2,18 +2,19 @@ import { useId } from 'react';
 
 export interface CheckboxProps {
 	label?: string,
+	checked?: boolean,
 	onChange?: () => void
 }
 
-export default function Checkbox({ label, onChange }: CheckboxProps) {
+export default function Checkbox({ checked, label, onChange }: CheckboxProps) {
 
 	const id = useId();
 
 	return (
 		<div className='flex gap-2 items-center'>
-			<input name={id} type='checkbox' className='w-4 h-4' onChange={onChange}/>
+			<input name={id} type='checkbox' checked={checked} className='w-4 h-4' onChange={onChange}/>
 			{ label &&
-				<label htmlFor={id} className='text-gray-600'>
+				<label htmlFor={id} className='text-gray-500'>
 					{ label }
 				</label> }
 		</div>
--- Source Code/Admin/src/features/scoring/Scoring.tsx
+++ Source Code/Admin/src/features/scoring/Scoring.tsx
@@ -4,33 +4,114 @@ import PageHeading from '../../components/PageHeading';
 import Image from '../../components/Image';
 import { useQuery } from '@tanstack/react-query';
 import { fetchDrawingById, fetchImage } from '../../api/sharedDrawing.api';
+import H2Heading from '../../components/H2Heading';
+import { useReducer, useState } from 'react';
+import ScoreSelector from './ScoreSelector';
+
+export interface TopicState {
+	checked: boolean,
+	topic: {
+		id: number,
+		name: string
+	},
+	extent: number,
+	extentNotes: string,
+	depth: number,
+	depthNotes: string
+}
+
+export interface ScoreState {
+	notes: string,
+	topics: TopicState[]
+}
+
+export const ScoreStateActionType = {
+	CHECK: 'CHECK',
+	UPDATE_NOTES: 'UPDATE_NOTES',
+	UPDATE_DEPTH: 'UPDATE_DEPTH',
+	UPDATE_EXTENT: 'UPDATE_EXTENT',
+	UPDATE_DEPTH_NOTES: 'UPDATE_DEPTH_NOTES',
+	UPDATE_EXTENT_NOTES: 'UPDATE_EXTENT_NOTES'
+} as const
+
+export type ScoreStateAction = 
+	  { type: "CHECK", index: number }
+	| { type: "UPDATE_NOTES", value: string }
+
+const reducer = (state: ScoreState, action: ScoreStateAction) => {
+	switch (action.type) {
+		case ScoreStateActionType.CHECK:
+			return {
+				...state,
+				topics: [
+					...state.topics.slice(0, action.index),
+					{ ...state.topics[action.index], checked: !state.topics[action.index].checked },
+					...state.topics.slice(action.index + 1, state.topics.length)
+				]
+			}
+		case ScoreStateActionType.UPDATE_NOTES:
+			return {
+				...state,
+				notes: action.value
+			}
+		default:
+			return state;
+	}
+}
 
 export default function Scoring() {
 
+	const [section, setSection] = useState<number>(-1);
+
+	const [scores, dispatch] = useReducer(reducer, {
+		notes: '',
+		topics: [
+			{ checked: false, topic: { id: 1, name: 'Marine Fauna' }, extent: 0, depth: 0, extentNotes: '', depthNotes: '' },
+			{ checked: false, topic: { id: 2, name: 'Marine Flora' }, extent: 0, depth: 0, extentNotes: '', depthNotes: '' },
+			{ checked: false, topic: { id: 3, name: 'Plymouth Landmarks' }, extent: 0, depth: 0, extentNotes: '', depthNotes: '' },
+			{ checked: false, topic: { id: 4, name: 'Physical Features' }, extent: 0, depth: 0, extentNotes: '', depthNotes: '' },
+			{ checked: false, topic: { id: 5, name: 'Marine Threats' }, extent: 0, depth: 0, extentNotes: '', depthNotes: '' },
+			{ checked: false, topic: { id: 6, name: 'Marine Industry/Careers' }, extent: 0, depth: 0, extentNotes: '', depthNotes: '' },
+			{ checked: false, topic: { id: 7, name: 'Recreational Activities' }, extent: 0, depth: 0, extentNotes: '', depthNotes: ''}
+		]
+	});
+
 	const { id } = useParams();
 	const { data: drawing } = useQuery(['drawing', Number(id)], () => fetchDrawingById(Number(id)));
-	const { data: image, isLoading: isImageLoading } = useQuery(['image', Number(id)], () => fetchImage(drawing?.imageUrl ?? ''), { enabled: !!drawing });
+	const { data: image, isLoading: isImageLoading } = useQuery(['image', Number(id)], () => fetchImage(drawing?.imageUrl ?? ''), { enabled: !!drawing });	
+
+	const onSubmit = () => {
+		console.log(1);
+	}
+
+	const scoreSelectorProps = {
+		scores,
+		dispatch,
+		section,
+		setSection,
+		onSubmit
+	}
+
+	console.table(scores);
 
 	return (
 		<Content>
 
 			<PageHeading> Scoring </PageHeading>
 
-			<div className='mt-7 bg-white flex-grow rounded-md border-gray-200 border flex p-5 gap-5'>
+			<div className='mt-7 bg-white flex-grow rounded-md border-gray-200 border flex p-5'>
 				
-				<div className='flex-grow'>
+				<div className='flex-grow pr-5'>
 
-				<h2 className='text-xl text-gray-700'> Drawing </h2>
-				<h3 className='text-gray-600 mb-5'> { id } </h3>
+				<H2Heading> Drawing </H2Heading>
+				<h3 className='text-lg text-gray-600 mb-5'> { id } </h3>
 				
 				<Image isLoading={isImageLoading} url={image}/>
 				</div>
 
 				<div className='h-full border-l border-gray-200'/>
 
-				<div className='w-80 h-full overflow-y-auto'>
-					
-				</div>
+				<ScoreSelector {...scoreSelectorProps}/>
 
 			</div>
 
