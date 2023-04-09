49f1df2d3c7aa78856f86498c9a89896ecddf47b
Justas Galminas <j.galminas@gmail.com>
2023-04-09 11:37:00 +0100

    Created user context

---
+++ Source Code/Admin/src/contexts/UserContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+export type User = {
+	id: number,
+	username: string
+}
+
+export interface UserContextProps {
+	setUser: (user: User) => void,
+	user: User | null,
+	signOut: () => void
+}
+
+const UserContext = createContext<UserContextProps>({
+	setUser: () => {},
+	user: null,
+	signOut: () => {}
+});
+
+export const useUser = () => useContext(UserContext);
+
+export interface UserProviderProps {
+	children?: ReactNode
+}
+
+export function PageProvider({ children }: UserProviderProps) {
+
+	const [user, setUser] = useState<User | null>(null);
+
+	const value: UserContextProps = {
+		setUser,
+		user,
+		signOut: () => setUser(null)
+	}
+
+	return (
+		<UserContext.Provider value={value}>
+			{ children }
+		</UserContext.Provider>
+	)
+}
