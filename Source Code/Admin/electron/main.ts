acca7db45b027bce5049cbcb79d80e132c0b9d46
Justas Galminas <j.galminas@gmail.com>
2023-04-18 15:15:30 +0100

    remove toolbar & maximise admin app when in prod

--- Source Code/Admin/electron/main.ts
+++ Source Code/Admin/electron/main.ts
@@ -43,14 +43,14 @@ async function createWindow() {
     },
   })
 
-  // win.maximize();
-
   if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
     win.loadURL(url)
     // Open devTool if the app is not packaged
     win.webContents.openDevTools()
   } else {
-    win.loadFile(indexHtml)
+    win.loadFile(indexHtml);
+    win.removeMenu();
+    win.maximize();
   }
 
   // Test actively push message to the Electron-Renderer
